faaf64ff18d2e7f809e2ff6da98ed2c2865152d7
JTH <nex_david@naver.com>
2023-09-23 22:01:17 +0900

    :sparkles: like_post 앱 url 등록 (#5)

--- post/urls.py
+++ post/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('get-crew-info/', get_crew_info, name='get-crew-info'), # 1번
+    path('get-crew-info/', get_crew_info, name='get-crew-info'),  # 1번
+    path('like-post/', like_post, name='like-post'),              # 2번
 ]
     
