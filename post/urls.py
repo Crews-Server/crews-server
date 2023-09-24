a1adf8e0e94a30698d04a63289f7d9832af9b442
JTH <nex_david@naver.com>
2023-09-25 02:04:36 +0900

    :sparkles: post_content url 처리 완료 (#5)

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('get-crew-info/', get_crew_info, name='get-crew-info'),  # 1번
     path('like-post/', like_post, name='like-post'),              # 2번
+    path('post-content/', post_content, name='post-content'),     # 3번
 ]
     
