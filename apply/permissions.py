19a98115af10b96249f9023c02718028e7bdbec0
jongmee <ra123221@gmail.com>
2023-10-04 15:34:43 +0900

    :sparkles: 동아리 운영진 권한을 갖지 않았을 때 반환하는 메세지 커스텀하기 (#9)

--- apply/permissions.py
+++ apply/permissions.py
@@ -1,7 +1,10 @@
 from rest_framework import permissions
+from rest_framework.exceptions import PermissionDenied
 
 # 동아리 운영진에 대한 permission
 class IsAdministrator(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_operator)
+        if request.user and request.user.is_operator:
+            return True
+        raise PermissionDenied(detail="Request user is not crew administrator")
