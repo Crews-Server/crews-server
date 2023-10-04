21a7782a2b53dc9a847fa1a50e354adb14f779da
jongmee <ra123221@gmail.com>
2023-10-04 14:24:07 +0900

    :sparkles: 지원서 문항 생성을 위한 serializer들 생성하기 (#9)

--- apply/serializers.py
+++ apply/serializers.py
@@ -1,7 +1,32 @@
 from rest_framework import serializers
-from table.models import Post
+from table.models import Post, Section, LongSentence, CheckBox, File, CheckBoxOption
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         exclude = ['pass_message', 'fail_message', 'created_at']
+
+class SectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Section
+        fields = '__all__'
+
+class LongSentenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LongSentence
+        fields = '__all__'
+
+class CheckBoxSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CheckBox
+        fields = '__all__'
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = '__all__'
+
+class CheckBoxOptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CheckBoxOption
+        fields = '__all__'
