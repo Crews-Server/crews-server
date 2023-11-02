18d71522b61f5310483339d64b21c763a4fd08f1
JTH <nex_david@naver.com>
2023-11-02 21:42:23 +0900

    :sparkles: 일반 유저의 마이페이지에서 본인이 지원한 동아리 리스트 반환해주는 api 개발 완료 (#6)

--- mypage/urls.py
+++ mypage/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('get-user-info/', get_user_info, name='get-user-info'),                 # 1번
-    # path                                                                       # 2번
+    path('get-applied-list/', get_applied_list, name="get-applied-list"),                                                                 # 2번
     path("get-liked-post/", get_liked_post, name="get-liked-post"),              # 3번
 ]
