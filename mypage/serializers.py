3ba125cb5d14a05adf163a94e8fbf04dc6d8c0c6
JTH <nex_david@naver.com>
2023-11-18 16:16:33 +0900

    :bug: mypage 관련 api들 버그 전부 수정하기 (#6)

--- mypage/serializers.py
+++ mypage/serializers.py
@@ -45,7 +45,8 @@ class GetAppliedListSerializer(serializers.ModelSerializer):
             return "2차 결과 확인" 
         else:
             return "모집 종료"
-        
+
+
     def get_category_name(self, obj):
         crew = obj.crew
         category = crew.category
@@ -61,12 +62,45 @@ class GetLikedPostSerializer(serializers.ModelSerializer):
     category_name = serializers.SerializerMethodField()
     class Meta:
         model = Post
-        fields = ['id','crew_name', 'category_name', 'apply_end_date', 'title',]   
+        fields = ['id', 'crew', 'category_name', 'crew_name', 'title', 'apply_end_date', 'button_status',]  
         # 동아리 이름, 지원서 마감 기한, 모집글 제목
 
     def get_crew_name(self, obj):
         return obj.crew.crew_name
     
+    def get_button_status(self, obj):
+        user = self.context.get('user')   # user 받아오기
+        now = timezone.now()
+
+        try:
+            like = Like.objects.get(user=user, post = obj)
+        except ObjectDoesNotExist:
+            return 0
+        
+        try:
+            apply = Apply.objects.get(user=user, post = obj)
+        except ObjectDoesNotExist:
+            # 찜은 했으나 아직 지원은 안 한 경우
+            if now < obj.apply_end_date:
+                return "지원하기"
+            elif obj.apply_end_date <= now and now < obj.document_result_date:     # 서류 마감시간부터 서류(1차) 합격자 발표시간 사이일 때
+                return "지원 기간 아님"
+            else:
+                return "모집 종료"
+
+        # 찜도 하고 지원도 한 경우!
+        if now < obj.apply_end_date:
+            return "지원서 수정하기"
+        elif obj.apply_end_date <= now and now < obj.document_result_date: # 서류 마감시간부터 서류(1차) 합격자 발표시간 사이일 때
+            return "지원 기간 아님"
+        elif obj.document_result_date <= now and now < obj.final_result_date:  # 1차 발표부터 최종 2차(최종)발표 사이까지
+            return "1차 결과 확인"
+        elif obj.final_result_date <= now and apply.document_pass == True:  # 2차 발표 시간 이후이면서 1차 합격한 사람의 경우
+            return "2차 결과 확인" 
+        else:
+            return "모집 종료"
+
+
     def get_category_name(self, obj):
         crew = obj.crew
         category = crew.category
