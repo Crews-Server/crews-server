c60632d2886e5e012b893af2d88be332898221c3
JTH <nex_david@naver.com>
2023-11-18 22:25:01 +0900

    :sparkles: 1~5번 api 내용 한번에 보내주는 all-in-one api 만들기 (#6)

--- mypage/urls.py
+++ mypage/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('get-applied-list/', get_applied_list, name="get-applied-list"),                      # 3번                                                        
     path("get-liked-post/", get_liked_post, name="get-liked-post"),                            # 4번
     path("get-crews-posts/", get_crews_posts, name="get-crews-posts"),                         # 5번
+    path('get-all-mypage-info/', get_all_mypage_info, name="get-all-mypage-info")              # 6번
 ]
