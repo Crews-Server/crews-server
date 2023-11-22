9b492005d16eeb740cd64ab802de75a6de5891ed
JTH <nex_david@naver.com>
2023-11-22 11:43:10 +0900

    :sparkles: 메인페이지 모든 구현 완료 (#44)

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    
+    path('normal-get-main/', normal_get_main, name="normal-get-main"), # 1번
+    path('search-post/', search_post, name="search-post"), # 2번
 ]
