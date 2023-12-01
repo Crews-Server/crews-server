44d77edb141a1dc54b2afc7e548b1a97fe7a5024
jongmee <ra123221@gmail.com>
2023-12-01 16:41:23 +0900

    :sparkles: get_object_or_404 반환 에러 커스텀하기 (#60)

---
+++ utils/get_object.py
@@ -0,0 +1,10 @@
+from django.http import Http404
+from rest_framework.generics import get_object_or_404
+
+from config.exception.custom_exception import NotFountExeption
+
+def custom_get_object_or_404(queryset, *filter_args, **filter_kwargs):
+    try:
+        return get_object_or_404(queryset, *filter_args, **filter_kwargs)
+    except Http404:
+        raise NotFountExeption(queryset.get_name())
