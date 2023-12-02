726e2a5c7145ee31034b834ab562fc99a3137709
jongmee <ra123221@gmail.com>
2023-12-02 21:42:02 +0900

    :sparkles: 개발 환경에서 media 경로 설정하기 (#64)

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,8 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from .settings import DEBUG, MEDIA_URL, MEDIA_ROOT
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,3 +13,6 @@ urlpatterns = [
     path('mypage/', include('mypage.urls')),
     path('post/', include('post.urls')),
 ]
+
+if DEBUG:
+    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
