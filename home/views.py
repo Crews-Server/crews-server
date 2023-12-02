0ec454a9fecfcfdd3d956a264c7068d53e831a78
jongmee <ra123221@gmail.com>
2023-12-03 01:18:26 +0900

    :sparkles: 모집 공고 조회 기능에 카테고리 필터 검색 기능 추가하기 (#64)

--- home/views.py
+++ home/views.py
@@ -1,14 +1,11 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.db.models import Q
 from django.utils import timezone
-from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
 
 from .serializers import MainSerializer
-from table.models import Post, Category, Crew
+from table.models import Post
 
 
 # 메인 페이지 조회 api
@@ -23,8 +20,16 @@ class Main(ListAPIView):
     search_fields = ['title', 'crew__crew_name']
 
     def get_queryset(self):
-        ordering = self.request.query_params.get('ordering', 'apply_end_date')
         qs = super().get_queryset()
+
+        categories = self.request.query_params.getlist('category')
+        if categories:
+            category_query = Q()
+            for category in categories:
+                category_query |= Q(crew__category__category_name=category, crew__category__isnull=False)
+            qs = qs.filter(category_query)
+
+        ordering = self.request.query_params.get('ordering', 'apply_end_date')
         if ordering == 'apply_end_date':
             return qs.filter(apply_end_date__gte=timezone.now().date()).order_by(ordering)
         return qs.order_by('-'+ordering)
