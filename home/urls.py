46f650a7f49062df3cddde72dd766c81559df987
jongmee <ra123221@gmail.com>
2023-12-03 00:42:56 +0900

    :sparkles: 메인 페이지에서 모집 공고를 조회할 때 정렬과 검색 기능 구현하기 (#64)

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
-from .views import *
+from django.urls import path
+from .views import Main
 
 urlpatterns = [
-    path('normal-get-main/', normal_get_main, name="normal-get-main"), # 1번
-    path('search-post/', search_post, name="search-post"), # 2번
+    path('', Main.as_view(), name="main-page"), # 1번
 ]
